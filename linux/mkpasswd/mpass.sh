8e69bac1be73ae07477e74d4bf5ec45552670e80
BruceZ <363401639@qq.com>
2015-06-29 11:42:51 +0800

    scripts

---
+++ linux/mkpasswd/mpass.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#pssh.txt:Host IP
+#Autor:Geekwolf
+while read line 
+do
+a=`mkpasswd -l 16 -c 2 -C 2 -s 1 -v`
+echo  -e  "$line \t  $a">>nowpassw.txt
+pssh -H $line   -P  -p 1 "echo $a|passwd --stdin root"
+echo $line
+done < pssh.txt
